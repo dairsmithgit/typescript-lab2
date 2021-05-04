681944d7fd03ae1ce651c21bc2595a62d13eede6
Dair Smith <dair.code@gmail.com>
2021-05-04 15:38:35 -0400

    almost done with loud greeter

---
+++ loudgreeter.test.ts
@@ -0,0 +1,17 @@
+import { LoudGreeter } from "./loudgreeter";
+
+describe("LoudGreeter", () => {
+
+    test("starts with 2 volume", () => {
+        let loudGreeting: LoudGreeter = new LoudGreeter("Buenas Dias");
+        let result: string = loudGreeting.greet("Mandy");
+        expect(result).toBe("Buenas Dias, Mandy!!");
+    });
+
+    test("calling volume makes 3", () => {
+        let loudGreeting: LoudGreeter = new LoudGreeter("Buenas Dias");
+        let result: string = loudGreeting.greet("Mandy");
+        loudGreeting.addVolume();
+        expect(result).toBe("Buenas Dias, Mandy!!!");
+    });
+});
---
+++ loudgreeter.ts
@@ -0,0 +1,16 @@
+import { Greeter } from "./greeter";
+
+export class LoudGreeter extends Greeter {
+
+    constructor(greeting: string, private extra: string = "!") {
+        super(greeting);
+        this.extra = extra;
+    }
+
+    addVolume(): void {
+        this.extra + "!";
+    }
+    greet(name: string) {
+        return super.greet(name) + this.extra;
+    }
+}
